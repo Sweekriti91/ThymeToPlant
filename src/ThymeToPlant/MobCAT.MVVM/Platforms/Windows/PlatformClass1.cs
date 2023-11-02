6eb83b79f10aa92d08abe0f0dbd614cfe9140fbc
Sweekriti Satpathy <swsat@microsoft.com>
2023-11-01 19:40:30 -0700

    delete extra

--- src/ThymeToPlant/MobCAT.MVVM/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MobCAT.MVVM;
-
-// All the code in this file is only included on Windows.
-public class PlatformClass1
-{
-}
-
--- src/ThymeToPlant/MobCAT.MVVM/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MobCAT.MVVM;
-
-// All the code in this file is only included on iOS.
-public class PlatformClass1
-{
-}
-
